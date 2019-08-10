396f96e63fbb35b953ff50b53607800a7cc40523
Chris Lema <chris@chrislema.com>
2019-08-10 14:35:53 -0700

    Update config.js

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -2,7 +2,7 @@ const config = require('./data/config.json');
 
 module.exports = Object.assign(config, {
   head: [
-    ['link', { rel: 'icon', href: '/favicon.png' }]
+    ['link', { rel: 'icon', href: 'https://res.cloudinary.com/chrislema/image/upload/v1565472874/favicon_sy8a1m.png' }]
   ],
   markdown: {
     anchor: {
